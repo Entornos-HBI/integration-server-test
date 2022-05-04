738f4a082a38f8fdcfbf49dbc08b0af771b32341
hectorvm123 <hectorvallsmira2003@gmail.com>
2022-05-04 16:28:05 +0200

    setUpMyCalculator and tearDownMyCalculator created successfully

--- src/test/java/es/devtest/mvn/MyCalculatorTest.java
+++ src/test/java/es/devtest/mvn/MyCalculatorTest.java
@@ -1,15 +1,28 @@
 package es.devtest.mvn;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyCalculatorTest {
+    private MyCalculator myCalculator;
+
+    @BeforeEach
+    void setUpMyCalculator(){
+        myCalculator = new MyCalculator();
+        System.out.println("MyCalculator created");
+    }
+    @AfterEach
+    void tearDownMyCalculator(){
+        myCalculator = null;
+        System.out.println("MyCalculator=null");
+    }
 
     @Test
     void add() {
-        MyCalculator myCalculator = new MyCalculator();
-
+        System.out.println("====TEST SUMA====");
         int expectedResult = 10;
         int actualResult = myCalculator.add(5, 5);
 
@@ -18,8 +31,7 @@ class MyCalculatorTest {
 
     @Test
     void sub() {
-        MyCalculator myCalculator = new MyCalculator();
-
+        System.out.println("====TEST RESTA====");
         int expectedResult = 0;
         int actualResult = myCalculator.sub(5, 5);
 
@@ -28,8 +40,7 @@ class MyCalculatorTest {
 
     @Test
     void mult() {
-        MyCalculator myCalculator = new MyCalculator();
-
+        System.out.println("====TEST MULTIPLICACION====");
         int expectedResult = 25;
         int actualResult = myCalculator.mult(5, 5);
 
@@ -38,11 +49,9 @@ class MyCalculatorTest {
 
     @Test
     void div() {
-        MyCalculator myCalculator = new MyCalculator();
-
+        System.out.println("====TEST DIVISION====");
         int expectedResult = 1;
         int actualResult = myCalculator.div(5, 5);
-
         assertEquals(expectedResult, actualResult);
     }
 }
